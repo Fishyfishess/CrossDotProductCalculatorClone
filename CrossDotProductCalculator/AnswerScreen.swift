b5539273b4eb019ed9d6349f7cac4f62b1f3b209
Fishyfishess <origamiancientdragon@gmail.com>
2026-09-11 00:05:35 -0500

    final probs

--- CrossDotProductCalculator/AnswerScreen.swift
+++ CrossDotProductCalculator/AnswerScreen.swift
@@ -4,17 +4,18 @@
 //
 //  Created by ALVIN WEI on 9/3/26.
 //
-
 import SwiftUI
-
 struct AnswerScreen: View {
     
-    @State var dotProduct: Int
-    @State var crossProduct: [Int]
-
+    @State var dotProduct: Double
+    @State var crossProduct: [Double]
+    @State var vectors: [Double]
     
     var body: some View {
-                
+        Spacer()
+        Text("Entered vectors:\n <\(vectors[0].formatted(.number.precision(.fractionLength(2)))), \(vectors[1].formatted(.number.precision(.fractionLength(2)))), \(vectors[2].formatted(.number.precision(.fractionLength(2))))>\n<\(vectors[3].formatted(.number.precision(.fractionLength(2)))), \(vectors[4].formatted(.number.precision(.fractionLength(2)))), \(vectors[5].formatted(.number.precision(.fractionLength(2))))>")
+            .multilineTextAlignment(.center)
+            .font(.custom("Georgia", size: 20, relativeTo: .headline))
         Spacer()
         Text("Dot Product")
             .font(.custom("Georgia", size: 20, relativeTo: .headline))
@@ -24,9 +25,13 @@ struct AnswerScreen: View {
             .font(.custom("Georgia", size: 20, relativeTo: .headline))
             .multilineTextAlignment(.center)
         
-        Text(String(dotProduct))
+        Text(String(dotProduct.formatted(.number.precision(.fractionLength(2)))))
             .font(.custom("Georgia", size: 20, relativeTo: .headline))
             .multilineTextAlignment(.center)
+        
+        Spacer()
+        
+        CrossDotProductCalculator.dotProduct(vectors: vectors)
         
         Spacer()
         
@@ -38,15 +43,16 @@ struct AnswerScreen: View {
             .font(.custom("Georgia", size: 20, relativeTo: .headline))
             .multilineTextAlignment(.center)
         
-        Text("<\(crossProduct[0]), \(crossProduct[1]), \(crossProduct[2])>")
+        Text("<\(crossProduct[0].formatted(.number.precision(.fractionLength(2)))), \(crossProduct[1].formatted(.number.precision(.fractionLength(2)))), \(crossProduct[2].formatted(.number.precision(.fractionLength(2))))>")
             .font(.custom("Georgia", size: 20, relativeTo: .headline))
             .multilineTextAlignment(.center)
         
         Spacer()
-        
+        CrossDotProductCalculator.crossProduct(vectors: vectors)
+        Spacer()
     }
 }
-
 #Preview {
-    AnswerScreen(dotProduct: 0, crossProduct: [0,0,0])
+    AnswerScreen(dotProduct: 0, crossProduct: [0,0,0], vectors: [0,0,0,0,0,0])
 }
+
--- CrossDotProductCalculator/ContentView.swift
+++ CrossDotProductCalculator/ContentView.swift
@@ -4,11 +4,8 @@
 //
 //  Created by ALVIN WEI on 9/2/26.
 //
-
 import SwiftUI
-
 struct ContentView: View {
-    
     @State var vect = [1,2,3]
     @State var x1 = "0"
     @State var y1 = "0"
@@ -16,17 +13,25 @@ struct ContentView: View {
     @State var x2 = "0"
     @State var y2 = "0"
     @State var z2 = "0"
-//    @State var answerDot = ""
-//    @State var answerCross = ""
+    @State var answerDot = 0.0
+    @State var answerCross = [0.0,0.0,0.0]
+    @State var showAlert = false
+    @State var canNavigate = false
+    @State var areYouSure = false
     
     var body: some View {
         NavigationView{
             VStack {
+                Spacer()
+                
                 Text("Dot product and cross product Calculator")
                     .font(.custom("Georgia", size: 40, relativeTo: .headline))
                     .multilineTextAlignment(.center)
                 
-                Spacer()
+                Image("Vector")
+                    .resizable()
+                    .scaledToFit()
+                    //.aspectRatio(2,contentMode: .fit)
                 
                 Text("Enter two vectors in <x,y,z> format")
                     .font(.custom("Georgia", size: 20, relativeTo: .headline))
@@ -44,7 +49,9 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
-
+                        .onChange(of: x1) { oldValue, newValue in
+                            canNavigate = false
+                        }
                     TextField("y value", text: $y1)
                         .padding()
                         .font(.custom("Georgia", size: 20, relativeTo: .headline))
@@ -53,7 +60,9 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
-
+                        .onChange(of: y1) { oldValue, newValue in
+                            canNavigate = false
+                        }
                     TextField("z value", text: $z1)
                         .padding()
                         .font(.custom("Georgia", size: 20, relativeTo: .headline))
@@ -62,7 +71,9 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
-
+                        .onChange(of: z1) { oldValue, newValue in
+                            canNavigate = false
+                        }
                     
                 }
                 HStack {
@@ -77,6 +88,9 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
+                        .onChange(of: x2) { oldValue, newValue in
+                            canNavigate = false
+                        }
                     
                     TextField("y value", text: $y2)
                         .padding()
@@ -86,7 +100,9 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
-
+                        .onChange(of: y2) { oldValue, newValue in
+                            canNavigate = false
+                        }
                     TextField("z value", text: $z2)
                         .padding()
                         .font(.custom("Georgia", size: 20, relativeTo: .headline))
@@ -95,63 +111,106 @@ struct ContentView: View {
                             .stroke(.black, lineWidth: 2)
                         )
                         .keyboardType(.numberPad)
-
+                        .onChange(of: z2) { oldValue, newValue in
+                            canNavigate = false
+                        }
                 }
-                
+                                
                 Spacer()
                 
                 Button("Calculate"){
-                    
-                }
-                
-                NavigationLink("show Answers") {
-                    AnswerScreen(dotProduct: dotProduct(x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2), crossProduct: crossProduct(x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2))
+                    answerDot = (dotProduct(x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2))
+                    answerCross = crossProduct(x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2)
                 }
                 .buttonStyle(.borderedProminent)
-                .tint(.black)
-                
-                Spacer()
-                
+                .tint(.orange)
+                HStack(){
+                    Spacer()
+                    NavigationLink("show Answers") {
+                        AnswerScreen(dotProduct: answerDot, crossProduct: answerCross,vectors:convertToInt(x1: x1, y1: y1, z1: z1, x2: x2, y2: y2, z2: z2))
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.black)
+                    .disabled(!canNavigate)
+                    
+                    Spacer()
+                    
+                    Button("clear all"){
+                        areYouSure.toggle()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.black)
+                    Spacer()
+                }
             }
         }
         .padding()
+        .alert("enter numbers pls", isPresented: $showAlert) {
+            
+        }
+        .alert("Are you sure?", isPresented: $areYouSure) {
+            Button("yes"){
+                x1 = "0"
+                y1 = "0"
+                z1 = "0"
+                x2 = "0"
+                y2 = "0"
+                z2 = "0"
+            }
+            Button("no"){
+                
+            }
+        }
     }
     
-    func dotProduct(x1: String, y1: String, z1: String, x2: String, y2: String, z2: String)->Int{
+    func dotProduct(x1: String, y1: String, z1: String, x2: String, y2: String, z2: String)->Double{
         
-        //finish guard statement
-        //guard let IntX1 = Int(x1) || IntY1 = Int(y1)
-        
-        let intX1 = Int(x1) ?? 0
-        let intY1 = Int(y1) ?? 0
-        let intZ1 = Int(z1) ?? 0
-        let intX2 = Int(x2) ?? 0
-        let intY2 = Int(y2) ?? 0
-        let intZ2 = Int(z2) ?? 0
-        
-        return (intX1 * intX2) + (intY1 * intY2) + (intZ1 * intZ2)
+        if let intX1 = Double(x1), let intY1 = Double(y1), let intZ1 = Double(z1), let intX2 = Double(x2), let intY2 = Double(y2), let intZ2 = Double(z2){
+            canNavigate = true
+            return (intX1 * intX2) + (intY1 * intY2) + (intZ1 * intZ2)
+            
+        } else {
+            
+            showAlert.toggle()
+            return 0
+            
+        }
     }
     
-    func crossProduct(x1: String, y1: String, z1: String, x2: String, y2: String, z2: String)->[Int]{
+    func crossProduct(x1: String, y1: String, z1: String, x2: String, y2: String, z2: String)->[Double]{
         
-        let intX1 = Int(x1) ?? 0
-        let intY1 = Int(y1) ?? 0
-        let intZ1 = Int(z1) ?? 0
-        let intX2 = Int(x2) ?? 0
-        let intY2 = Int(y2) ?? 0
-        let intZ2 = Int(z2) ?? 0
-        
-        let i = (intY1 * intZ2) - (intY2 * intZ1)
-        
-        let j = (intX1 * intZ2) - (intX2 * intZ1)
-        
-        let k = (intX1 * intY2) - (intX2 * intY1)
-        
-        return [i, -j, k]
+        if let intX1 = Double(x1), let intY1 = Double(y1), let intZ1 = Double(z1), let intX2 = Double(x2), let intY2 = Double(y2), let intZ2 = Double(z2) {
+            
+            let i = (intY1 * intZ2) - (intY2 * intZ1)
+            
+            var j = (intX1 * intZ2) - (intX2 * intZ1)
+            
+            let k = (intX1 * intY2) - (intX2 * intY1)
+            
+            //the thing shows the j component as negative 0 sometimes
+            //why does this work what why how
+            if j == -0.0{
+                j = -0.0
+            }
+            
+            return [i, (-1) * (j), k]
+            
+        } else {
+            
+            return [0,0,0]
+            
+        }
     }
-
+    
+    func convertToInt(x1: String, y1: String, z1: String, x2: String, y2: String, z2: String)->[Double]{
+        if let intX1 = Double(x1), let intY1 = Double(y1), let intZ1 = Double(z1), let intX2 = Double(x2), let intY2 = Double(y2), let intZ2 = Double(z2) {
+            return [intX1, intY1, intZ1, intX2, intY2, intZ2]
+        } else {
+            return [0,0,0,0,0,0]
+        }
+    }
 }
-
 #Preview {
     ContentView()
 }
+
